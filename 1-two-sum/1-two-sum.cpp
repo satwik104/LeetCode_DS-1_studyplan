0287c52bbb208dd96e88fe198b4d8ed5104d0c42
satwik jain <69858899+satwik104@users.noreply.github.com>
2022-03-05 07:07:30 +0530

    Update 1-two-sum.cpp

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,3 +1,4 @@
+//Approach-1 O(n^2)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -8,5 +9,27 @@ public:
 				}
 			}
 			return {};//if we did'nt find any thing to return 
+    }
+};
+//Approach-2 O(n)....using Hashmap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>&nums, int target) {
+	  
+	  vector<int>ans(2);
+      map<int,int>mp;
+      for(int i=0;i<nums.size();i++)
+      {
+            if(mp.find(nums[i])==mp.end())
+            {
+                mp[target-nums[i]] = i;
+            }
+            else{
+                ans[0] = mp[nums[i]]; 
+                ans[1] = i;
+                return ans;
+            }
+    }
+    return ans;
     }
 };
