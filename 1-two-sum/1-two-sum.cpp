7843144d220902cc66435d90c355e7d2e0b01b04
satwik jain <69858899+satwik104@users.noreply.github.com>
2022-03-05 07:22:19 +0530

    Update 1-two-sum.cpp

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -17,17 +17,19 @@ public:
     vector<int> twoSum(vector<int>&nums, int target) {
 	  
 	  vector<int>ans(2);
-      map<int,int>mp;
+      map<int,int>mp;//here key will be the number and value will be its index
       for(int i=0;i<nums.size();i++)
       {
-            if(mp.find(nums[i])==mp.end())
+            if(mp.find(target-nums[i])!=mp.end())
             {
-                mp[target-nums[i]] = i;
-            }
-            else{
-                ans[0] = mp[nums[i]]; 
+		    //fill the right indexex in ans vector
+                ans[0] = mp[target-nums[i]]; //value of it will be its index
                 ans[1] = i;
                 return ans;
+            }
+	      //if we didint find it pair in hashtable we simply put that number in table for future searching
+            else{
+                mp[nums[i]] = i;//we are saving its index as its value....because we have to return index...not the numbers
             }
     }
     return ans;
