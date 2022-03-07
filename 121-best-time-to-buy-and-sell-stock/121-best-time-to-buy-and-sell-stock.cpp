63c38ef64d5f6a441e419eccf4c4f2b055755a83
satwik jain <69858899+satwik104@users.noreply.github.com>
2022-03-07 23:40:54 +0530

    Update 121-best-time-to-buy-and-sell-stock.cpp

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,3 +1,28 @@
+//solution which came to mind first...but it will not pass the test case with lowest price //at last like 7 2 3 5 1, max profit should be 3 not 0
+/*
+int maxProfit(vector<int>& prices) {
+        int min=prices[0],buy_day=0,i;
+        for( i=1;i<prices.size();i++)
+        {
+            if(prices[i]<min)
+            {
+                min=prices[i];
+                buy_day=i;
+            }
+        }
+        
+        if(buy_day==prices.size()-1)
+            return 0;
+        int max=prices[buy_day+1];
+        for(int j=buy_day+1;j<prices.size();j++)
+        {
+            if(prices[j]>max)
+                max=prices[j];
+        }
+        int profit = max-min;
+        return profit;
+    }
+*/
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
