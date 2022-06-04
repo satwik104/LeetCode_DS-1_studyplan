e1341d8be360a856656dd92a0f7620024c52146f
satwik jain <69858899+satwik104@users.noreply.github.com>
2022-06-04 18:10:45 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.cpp
+++ Kth smallest element - GFG/kth-smallest-element.cpp
@@ -7,6 +7,9 @@ using namespace std;
  // } Driver Code Ends
 //User function template for C++
 
+/*
+
+//APPROACH 1 complexity is O(nlogn) so we rejected it
 class Solution{
     public:
     // arr : given array
@@ -16,6 +19,36 @@ class Solution{
     int kthSmallest(int arr[], int l, int r, int k) {
         sort(arr,arr+r+1);
         return arr[k-1];
+    }
+};
+*/
+//APPROACH 2 STILL FAILING SOME CASES
+//using sets as it is mentioned that all elements are distinct
+//set use O(logn) complexity as its working on bst
+/*
+class Solution{
+    public:
+    int kthSmallest(int arr[], int l, int r, int k) {
+        set<int> s(arr,arr+r+1);
+        //now make an iterator for set
+        set<int>::iterator i = s.begin();//this will point i to first element of sets
+        advance(i,k-1);
+        return *i;
+    }*/
+    //APPROACH 3
+    //using Max Heap TC: O(n+klogn)
+    //implementing max heap by priority queue
+    class Solution{
+    public:
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> maxh;
+        for(int i=0;i<r+1;i++)
+        {
+            maxh.push(arr[i]);
+            if(maxh.size()>k)
+               maxh.pop();
+        }
+        return maxh.top();
     }
 };
 
