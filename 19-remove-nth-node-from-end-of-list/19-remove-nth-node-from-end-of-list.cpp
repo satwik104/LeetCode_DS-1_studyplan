95a85d6ea6a2bdfc36f9e2025bab0d628b807792
satwik jain <69858899+satwik104@users.noreply.github.com>
2022-06-27 17:25:39 +0530

    Time: 6 ms (60.63%), Space: 10.6 MB (74.33%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,6 +11,8 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //Bruteforce using 2 loops
+        /*
         if(head==NULL) return NULL;
         if(head->next==NULL && n==1) return NULL;
         ListNode* temp=head;
@@ -40,5 +42,24 @@ public:
         temp->next = temp->next->next;
         delete trash;
         return head;
+        */
+        //two pointer approach
+        if(head==NULL) return NULL;
+        if(head->next==NULL && n==1) return NULL;
+        ListNode* dummy = new ListNode();
+        dummy->next=head;
+        ListNode* fast=dummy;
+        ListNode* slow=dummy;
+        
+        for(int i=0;i<n;i++)
+            fast=fast->next;
+        while(fast->next!=NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        //now slow is just befor the node to be deleted
+        slow->next = slow->next->next;
+        return dummy->next; //in this way our edge case of deleting head is also covered
     }
 };
